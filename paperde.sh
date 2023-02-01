d008248bba7430a320bf257427e8a437697b8443
thomasbonsirven <thomasbonsirven@gmail.com>
2023-02-01 15:16:52 +0100

    Update 0.1
    - Dependencies final
    - Compile PaperDE 
    - Install PaperDE

--- paperde.sh
+++ paperde.sh
@@ -26,21 +26,24 @@ apk add --no-cache \
 		qt5-qttranslations \
 		qt5-qtgraphicaleffects 
 
-apk add git meson wlroots ninja qt5-qtbase-x11 qt5-qtbase qt5-qtbase-dev qt5-qtwayland qt5-qtwayland-dev qt5-qtsvg qt5-qtsvg-dev qt5-qttools-dev libdbusmenu-qt-dev wayland-protocols wayland libcprime libcprime-dev libcsys libcsys-dev xdg-desktop-portal xdg-desktop-portal-kde xdg-desktop-portal-gtk xdg-desktop-portal-wlr make g++ swaylock swayidle brightnessctl mako playerctl 
+apk add git meson wlroots ninja qt5-qtbase-x11 qt5-qtbase qt5-qtbase-dev qt5-qtwayland qt5-qtwayland-dev qt5-qtsvg qt5-qtsvg-dev qt5-qttools-dev libdbusmenu-qt-dev wayland-protocols wayland libcprime libcprime-dev libcsys libcsys-dev xdg-desktop-portal xdg-desktop-portal-kde xdg-desktop-portal-gtk xdg-desktop-portal-wlr make g++ swaylock swayidle brightnessctl mako playerctl cmake
 
 echo Edge repository
 apk add wayfire --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
 apk add clipman --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
+cd ..
 
 echo Installation of WayQT
 git clone https://gitlab.com/desktop-frameworks/wayqt.git wayqt
 cd wayqt && meson .build --prefix=/usr --buildtype=release
 ninja -C .build -k 0 -j $(nproc) && ninja -C .build install
+cd ..
 
 echo Installation of DFL Applications
 git clone https://gitlab.com/desktop-frameworks/applications dfl-applications
 cd dfl-applications && meson .build --prefix=/usr --buildtype=release -Duse_qt_version=qt5
 ninja -C .build -k 0 -j $(nproc) && ninja -C .build install
+cd ..
 
 echo Installation of DFL IPC
 git clone https://gitlab.com/desktop-frameworks/ipc.git df5ipc
@@ -51,17 +54,26 @@ echo Installation of DFL Login1
 git clone https://gitlab.com/desktop-frameworks/login1.git dfl-login1
 cd dfl-login1 && meson .build --prefix=/usr --buildtype=release
 ninja -C .build -k 0 -j $(nproc) && ninja -C .build install
+cd ..
 
 echo Installation of DFL Settings
 git clone https://gitlab.com/desktop-frameworks/settings.git dfl-settings
 cd dfl-settings && meson .build --prefix=/usr --buildtype=release
 ninja -C .build -k 0 -j $(nproc) && ninja -C .build install
+cd ..
 
 echo Installation of DFL SNI
 git clone https://gitlab.com/desktop-frameworks/status-notifier.git dfl-sni
 cd dfl-sni && meson .build --prefix=/usr --buildtype=release
 ninja -C .build -k 0 -j $(nproc) && ninja -C .build install
+cd ..
 
 
+echo Installation PaperDE
+git clone https://gitlab.com/cubocore/paper/paperde paperde
+cd paperde && mkdir build && cd build
 
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DPKGCONFPATH=/etc/xdg/paperde -DPKGSHAREDPATH=share/paperde -GNinja ..
+ninja -k 0 -j $(nproc)
+ninja install
 

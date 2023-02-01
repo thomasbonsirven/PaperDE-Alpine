45a8f50c1395a53a83044a45f9bcc42b05670526
thomasbonsirven <thomasbonsirven@gmail.com>
2023-02-01 14:58:34 +0100

    Initial

---
+++ paperde.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo Installation Dependency
+apk add --no-cache \
+		qt5-qtdeclarative-dev \
+		qt5-qtwebsockets-dev \
+		qt5-qtwebchannel-dev \
+		qt5-qtwebengine-dev \
+		qt5-qtconnectivity-dev \
+		qt5-qtcharts-dev \
+		qt5-qtvirtualkeyboard-dev \
+		qt5-qtlocation-dev \
+		qt5-qtquickcontrols2-dev \
+		qt5-qtxmlpatterns-dev \
+		qt5-qtx11extras-dev \
+		qt5-qtserialport-dev \
+		qt5-qtsensors-dev \
+		qt5-qtmultimedia-dev \
+		qt5-qtspeech-dev \
+		qt5-qtremoteobjects-dev \
+		qt5-qtscript-dev \
+		qt5-qtimageformats \
+		qt5-qttranslations \
+		qt5-qtgraphicaleffects 
+
+apk add git meson wlroots ninja qt5-qtbase-x11 qt5-qtbase qt5-qtbase-dev qt5-qtwayland qt5-qtwayland-dev qt5-qtsvg qt5-qtsvg-dev qt5-qttools-dev libdbusmenu-qt-dev wayland-protocols wayland libcprime libcprime-dev libcsys libcsys-dev xdg-desktop-portal xdg-desktop-portal-kde xdg-desktop-portal-gtk xdg-desktop-portal-wlr make g++ swaylock swayidle brightnessctl mako playerctl 
+
+echo Edge repository
+apk add wayfire --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
+apk add clipman --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
+
+echo Installation of WayQT
+git clone https://gitlab.com/desktop-frameworks/wayqt.git wayqt
+cd wayqt && meson .build --prefix=/usr --buildtype=release
+ninja -C .build -k 0 -j $(nproc) && sudo ninja -C .build install
+
+echo Installation of DFL Applications
+git clone https://gitlab.com/desktop-frameworks/applications dfl-applications
+cd dfl-applications && meson .build --prefix=/usr --buildtype=release -Duse_qt_version=qt5
+ninja -C .build -k 0 -j $(nproc) && sudo ninja -C .build install
+
+echo Installation of DFL IPC
+git clone https://gitlab.com/desktop-frameworks/ipc.git df5ipc
+cd df5ipc && meson .build --prefix=/usr --buildtype=release
+ninja -C .build -k 0 -j $(nproc) && sudo ninja -C .build install
+
+echo Installation of DFL Login1
+git clone https://gitlab.com/desktop-frameworks/login1.git dfl-login1
+cd dfl-login1 && meson .build --prefix=/usr --buildtype=release
+ninja -C .build -k 0 -j $(nproc) && sudo ninja -C .build install
+
+echo Installation of DFL Settings
+git clone https://gitlab.com/desktop-frameworks/settings.git dfl-settings
+cd dfl-settings && meson .build --prefix=/usr --buildtype=release
+ninja -C .build -k 0 -j $(nproc) && sudo ninja -C .build install
+
+echo Installation of DFL SNI
+git clone https://gitlab.com/desktop-frameworks/status-notifier.git dfl-sni
+cd dfl-sni && meson .build --prefix=/usr --buildtype=release
+ninja -C .build -k 0 -j $(nproc) && sudo ninja -C .build install
+
+
+
+
+
